d95f1229052ed17ea0032410c53093e0539f0d11
maydayv7 <maydayv7@gmail.com>
2025-06-07 16:54:32 +0530

    chore: Update Packages
    - `hyprshellevents`: bc2ad24e0b2e66c3e164994c4897cd94a933fd10 ---> 910dad4c5755c1735d30da10c96d9086aa2a608d
    - `kitty-search`: 0760138fad617c5e4159403cbfce8421ccdfe571 ---> 992c1f3d220dc3e1ae18a24b15fcaf47f4e61ff8
    - `firefox-elementary`: 63a5c423db5d442117535aff16ecba73f59c5fa8 ---> 6e69ba722ac53762f427e21b199a46fc225028df

--- packages/firefox-elementary/metadata.nix
+++ packages/firefox-elementary/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://github.com/Zonnev/elementaryos-firefox-theme";
   branch = "elementaryos-firefox-theme";
-  rev = "63a5c423db5d442117535aff16ecba73f59c5fa8";
-  sha256 = "sha256-eVXYrv4U8FC8VnLZoa4rgkfNu46cKqJKYXqGCtt+gLY=";
+  rev = "6e69ba722ac53762f427e21b199a46fc225028df";
+  sha256 = "sha256-NxYfFSItptR9mPvf2nYRFWfA/7p1gmI7DDjPNhwUKc8=";
 }
--- packages/kitty-search/metadata.nix
+++ packages/kitty-search/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://github.com/trygveaa/kitty-kitten-search";
   branch = "master";
-  rev = "0760138fad617c5e4159403cbfce8421ccdfe571";
-  sha256 = "sha256-egisza7V5dWplRYHIYt4bEQdqXa4E7UhibyWJAup8as=";
+  rev = "992c1f3d220dc3e1ae18a24b15fcaf47f4e61ff8";
+  sha256 = "sha256-Xy4dH2fzEQmKfqhmotVDEszuTqoISONGNfC1yfcdevs=";
 }
